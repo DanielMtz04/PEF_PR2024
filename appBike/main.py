10926781b9f51c6d8fdade2e43188a54c0753da4
DiegoP <diego.perezl@udem.edu>
2024-02-18 23:30:55 -0600

    cambio de popup por dialog

--- appBike/main.py
+++ appBike/main.py
@@ -3,6 +3,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.logger import Logger
 from kivy.utils import platform
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton, MDRaisedButton
+
 import os
 import sys
 import asyncio
@@ -18,12 +21,12 @@ from AccHelper import AccHelper
 from GyroHelper import GyroHelper
 from kivy.factory import Factory
 
-
 # ADDRESS, UUID = "78:21:84:9D:37:10", "0000181a-0000-1000-8000-00805f9b34fb"
 ADDRESS, UUID = None, None
 GPS_ON = True
 DEBUG_GPS = False
 disconnect_flag = {'disconnect': False}
+
 
 class MainWindow(Screen): pass
 
@@ -38,7 +41,6 @@ class SpinnerDropdown(DropDown): pass
 
 
 class Main(MDApp):
-    
     dialog = None
     send_q = asyncio.Queue()
     speed_q = asyncio.Queue()
@@ -46,9 +48,41 @@ class Main(MDApp):
     battery_q = asyncio.Queue()
     angle_q = asyncio.Queue()
     acc_q = asyncio.Queue()
-    man_q = asyncio.Queue() 
+    man_q = asyncio.Queue()
 
-    
+    def exit_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                text="Desea salir?",
+                radius=[20, 7, 20, 7],
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        #theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_release=self.cancel_exit,
+                    ),
+                    MDRaisedButton(
+                        text="ACCEPT",
+                        #theme_text_color="Custom",
+                        #text_color=self.theme_cls.primary_color,
+                        on_release=self.accept_exit,
+                    ),
+                ],
+            )
+        self.dialog.open()
+
+    def cancel_exit(self, *args):
+        print("Navegando a screen 1...")
+        # self.root.current = "secondary_window"
+        self.root.current = "main_window"
+        self.dialog.dismiss()  # Cerrar el popup
+
+    def accept_exit(self, *args):
+        print("Cerrando app...")
+        self.stop()
+        #self.dialog.dismiss()  # Cerrar el popup
+
     screen_flag = True
 
     def screen_flag_1(self, touch: bool) -> None:
@@ -59,6 +93,28 @@ class Main(MDApp):
         self.screen_flag = False
         print(self.screen_flag)
 
+    def disconnect_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                text="Desea desconectar?",
+                radius=[20, 7, 20, 7],
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        # theme_text_color="Custom",
+                        text_color=self.theme_cls.primary_color,
+                        on_release=self.cancel_disconnect,
+                    ),
+                    MDRaisedButton(
+                        text="ACCEPT",
+                        # theme_text_color="Custom",
+                        # text_color=self.theme_cls.primary_color,
+                        on_release=self.accept_disconnect,
+                    ),
+                ],
+            )
+        self.dialog.open()
+
     def cancel_disconnect(self, touch: bool) -> None:
         if touch:  # Asegúrate de que este método solo se ejecute en respuesta a un evento de toque
             print("Navegando a screen 2...")
@@ -67,30 +123,30 @@ class Main(MDApp):
                 self.root.get_screen('secondary_window').ids.nav.switch_tab('screen 1')
             elif (self.screen_flag == False):
                 self.root.get_screen('secondary_window').ids.nav.switch_tab('screen 2')
-    '''            
+            self.dialog.dismiss()  # Cerrar el popup
+    '''
     def restart_app(self):
         """Reinicia la aplicación completamente."""
         python = sys.executable
         os.execv(python, ['python'] + sys.argv)
     '''
-
     def accept_disconnect(self, touch: bool) -> None:
-        
+
         if touch:
             self.root.current = 'main_window'
             asyncio.create_task(self.cancel_tasks())
+            self.root.get_screen('main_window').ids.spinner.active = False
             disconnect_flag['disconnect'] = True
             # Restablecer el estado de la UI y variables a sus valores iniciales
             self.root.get_screen('secondary_window').ids.nav.switch_tab('screen 1')
+
             self.reset_ui_and_variables()
-            
-            
+
+            self.dialog.dismiss()  # Cerrar el popup
 
     def reset_ui_and_variables(self):
         # Renames certain components from app that will be used in other functions
         self.button = self.root.get_screen('main_window').ids.ble_button
-        self.root.get_screen('main_window').ids.device_dropdown.text = ''
-        self.root.get_screen('main_window').ids.device_dropdown.disabled = True
         self.circle_bar = self.root.get_screen('secondary_window').ids.circle_progress
         self.speedmeter = self.root.get_screen('secondary_window').ids.speed
         self.an_button = self.root.get_screen('secondary_window').ids.angle_button
@@ -112,20 +168,9 @@ class Main(MDApp):
         self.slider_value = 0
         self.slider_flag = False
         self.test_counter = 0
-	
-	
-    def cancel_exit(self, touch: bool) -> None:
-        if touch:  # Asegúrate de que este método solo se ejecute en respuesta a un evento de toque
-            print("Navegando a screen 1...")
-            # self.root.current = "secondary_window"
-            self.root.current = "main_window"
+        self.root.get_screen('main_window').ids.device_dropdown.text = ''
+        self.root.get_screen('main_window').ids.device_dropdown.disabled = True
 
-    def accept_exit(self, touch: bool) -> None:
-        if touch:  # Asegúrate de que este método solo se ejecute en respuesta a un evento de toque
-            print("Cerrando app...")
-            self.stop()
-            
-            
     def build(self):
         """setting design for application widget development specifications on design.kv"""
         self.theme_cls.theme_style = 'Dark'
@@ -172,6 +217,8 @@ class Main(MDApp):
         self.slider_flag = False
         self.test_counter = 0
 
+        self.root.current= 'secondary_window'
+
     def get_permissions(self):
         # Request permissions on Android
         if platform == 'android':
@@ -194,25 +241,28 @@ class Main(MDApp):
     def connect_ble(self, touch: bool) -> None:
         """Function handling BLE connection between App and ESP32"""
         if touch:
-            self.root.get_screen('main_window').ids.spinner.active = False
             try:
                 if DEBUG_GPS:
                     self.root.current = 'secondary_window'
                 else:
-                    self.ble_task = asyncio.create_task(run_BLE(self, self.send_q, self.battery_q, self.drop_q, self.angle_q, self.man_q))
+                    self.ble_task = asyncio.create_task(
+                        run_BLE(self, self.send_q, self.battery_q, self.drop_q, self.angle_q, self.man_q))
                 self.gps_task = GpsHelper().run(self.speed_q)
                 self.update_battery_task = asyncio.ensure_future(self.update_battery_value())
                 self.update_speed_task = asyncio.ensure_future(self.update_speed_value())
                 self.update_angle_task = asyncio.ensure_future(self.update_angle_value())
                 self.update_manipulation_task = asyncio.ensure_future(self.update_manipulation_value())
-                
+
+
+
 
             except Exception as e:
                 print(e)
 
     async def cancel_tasks(self):
-        
-        tasks = [self.ble_task, self.gps_task, self.update_battery_task, self.update_speed_task, self.update_angle_task, self.update_manipulation_task, self.dropdown_clicked_task ]
+
+        tasks = [self.ble_task, self.gps_task, self.update_battery_task, self.update_speed_task, self.update_angle_task,
+                 self.update_manipulation_task, self.dropdown_clicked_task]
         for task in tasks:
             if task is not None:
                 task.cancel()
@@ -221,14 +271,13 @@ class Main(MDApp):
                 except asyncio.CancelledError:
                     print(f"{task.get_name()} was cancelled")
 
-
     def dropdown_clicked(self, _, value: str) -> None:
         """Handles events in main window dropdown"""
         self.dropdown_clicked_task = asyncio.ensure_future(self.dropdown_event_handler(value))
 
     async def dropdown_event_handler(self, value: str) -> None:
         await self.drop_q.put(value)
-        #self.root.get_screen('main_window').ids.spinner.active = True
+        self.root.get_screen('main_window').ids.spinner.active = True
 
     def switch_state(self, _, value: bool) -> None:
         """Switch state for adaptive mode switch. When in adaptive mode, the slider is disabled"""
@@ -380,6 +429,7 @@ class Main(MDApp):
                 print(f'Exception in battery:: {e}')
                 await asyncio.sleep(2.0)
 
+
 async def run_BLE(app: MDApp, send_q: asyncio.Queue, battery_q: asyncio.Queue, drop_q: asyncio.Queue,
                   angle_q: asyncio.Queue, man_q: asyncio.Queue) -> None:
     """Asyncronous connection protocol for BLE"""
@@ -396,7 +446,7 @@ async def run_BLE(app: MDApp, send_q: asyncio.Queue, battery_q: asyncio.Queue, d
                             app=app,
                             drop_q=drop_q)
     disconnect_flag['disconnect'] = False
-    
+
     try:
         asyncio.ensure_future(connection.manager())
         asyncio.ensure_future(communication_manager(connection=connection,
@@ -404,7 +454,7 @@ async def run_BLE(app: MDApp, send_q: asyncio.Queue, battery_q: asyncio.Queue, d
                                                     read_char=read_char,
                                                     send_q=send_q,
                                                     battery_q=battery_q, angle_q=angle_q,
-                                                    man_q=man_q,disconnect_flag=disconnect_flag))
+                                                    man_q=man_q, disconnect_flag=disconnect_flag))
         print(f"fetching connection")
         await connection.flag.wait()
     finally:
@@ -415,6 +465,21 @@ async def run_BLE(app: MDApp, send_q: asyncio.Queue, battery_q: asyncio.Queue, d
         app.root.current = 'secondary_window'
     except Exception as e:
         print(f'EXCEPTION WHEN CHANGING WINDOW -> {e}')
+
+
+async def disconnect_BLE(app: MDApp) -> None:
+    """Asynchronous protocol for BLE disconnection."""
+    print('Attempting to disconnect from BLE...')
+    try:
+        # Suponiendo que tienes una instancia de Connection accesible desde aquí.
+        # Si tu conexión está guardada de otra manera, ajusta esto acorde a tu implementación.
+        if app.connection:
+            await app.connection.cleanup()  # Asume que cleanup() es una corutina.
+            print('Disconnected successfully.')
+        else:
+            print('No BLE connection to disconnect.')
+    except Exception as e:
+        print(f'Exception during BLE disconnection: {e}')
 
 
 if __name__ == '__main__':
